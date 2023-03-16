d88ecf11873935709a27bba78dcb4a66fcbc8bcc
sc3d <117922845+python-nerd-3@users.noreply.github.com>
2023-03-16 19:07:43 -0400

    Add files via upload

---
+++ Section_1_HTML/Chapter_1.1_Basics/1.1.2_Check/script.js
@@ -0,0 +1,9 @@
+if ($("h1").length > 0) {
+    if ($("h1").html = "My First Website") {
+        alert("Great job!")
+    } else {
+        alert("You created a title, but it contains the wrong text. Try again.")
+    }
+} else {
+    alert("Could not find a title element. Try again.")
+}
